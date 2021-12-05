852154bd6610a5ce96516acc5a0aa4f0b5911479
Henrique Zolini <henriquezolini@gmail.com>
2021-12-04 21:28:41 -0300

    Adiciona nextjs e react

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+function Pepsi() {
+  return (
+    <div class="card">
+      <div class="circle"></div>
+      <div class="content">
+        <h2>Pepsi Cola</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          pellentesque mi nec mauris posuere elementum eu id eros!
+        </p>
+        <a href="#">Buy Now</a>
+      </div>
+      <img src="/pepsi.png" alt="" />
+    </div>
+  );
+}
+
+export default Pepsi;
